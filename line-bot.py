85fb559f5e7c1ab01409f8656716368f8302a516
BILLY CHANG <ts04574870@gmail.com>
2020-08-15 00:29:32 +0900

    update

--- line-bot.py
+++ line-bot.py
@@ -37,9 +37,15 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    msg = event.message.text
+    r = '意義不明解析錯誤'
+    if msg == 'hi':
+        r = 'hi'
+    elif msg == '你吃飯了嗎':
+        r = '還沒啦幹'
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
+        TextSendMessage(text=r))
 
 
 if __name__ == "__main__":
